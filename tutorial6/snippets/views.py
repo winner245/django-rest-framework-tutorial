147f3298d39357919fec1a1cd08ce59ad00036e9
Peng Liu <winner245@hotmail.com>
2021-12-02 23:38:31 -0500

    Refactoring to use ViewSets

--- tutorial6/snippets/views.py
+++ tutorial6/snippets/views.py
@@ -2,10 +2,12 @@ from django.contrib.auth.models import User
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics, permissions, renderers
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from .permissions import IsOwnerOrReadOnly
+
+from rest_framework import viewsets
 
 
 @api_view(['GET'])
@@ -14,39 +16,68 @@ def api_root(request, format=None):
         'users': reverse('user-list', request=request, format=format),
         'snippets': reverse('snippet-list', request=request, format=format)
     }) 
+    
+class SnippetViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
 
-class SnippetList(generics.ListCreateAPIView):
+    Additionally we also provide an extra `highlight` action.
+    """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    # IsAuthenticatedOrReadOnly: authenticated requests get read-write access, and unauthenticated requests get read-only access.
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    
-    # Override perform_create so that extra parameters are passed to the default create() method
-    def perform_create(self, serializer): 
-        serializer.save(owner=self.request.user)
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
 
-
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]  # IsOwnerOrReadOnly: custom object-level permission
-    
-
-class SnippetHighlight(generics.GenericAPIView):
-    queryset = Snippet.objects.all()
-    renderer_classes = [renderers.StaticHTMLRenderer]
-
-    def get(self, request, *args, **kwargs):
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
         snippet = self.get_object()
         return Response(snippet.highlighted)
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
-class UserList(generics.ListAPIView): 
-    queryset = User.objects.all() 
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAdminUser]
+
+# class SnippetList(generics.ListCreateAPIView):
+#     queryset = Snippet.objects.all()
+#     serializer_class = SnippetSerializer
+#     # IsAuthenticatedOrReadOnly: authenticated requests get read-write access, and unauthenticated requests get read-only access.
+#     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     
-class UserDetail(generics.RetrieveAPIView): 
-    queryset = User.objects.all() 
+#     # Override perform_create so that extra parameters are passed to the default create() method
+#     def perform_create(self, serializer): 
+#         serializer.save(owner=self.request.user)
+
+
+# class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Snippet.objects.all()
+#     serializer_class = SnippetSerializer
+#     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]  # IsOwnerOrReadOnly: custom object-level permission
+    
+
+# class SnippetHighlight(generics.GenericAPIView):
+#     queryset = Snippet.objects.all()
+#     renderer_classes = [renderers.StaticHTMLRenderer]
+
+#     def get(self, request, *args, **kwargs):
+#         snippet = self.get_object()
+#         return Response(snippet.highlighted)
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset automatically provides `list` and `retrieve` actions.
+    """
+    queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAdminUser]
+
+# class UserList(generics.ListAPIView): 
+#     queryset = User.objects.all() 
+#     serializer_class = UserSerializer
+#     permission_classes = [permissions.IsAdminUser]
+    
+# class UserDetail(generics.RetrieveAPIView): 
+#     queryset = User.objects.all() 
+#     serializer_class = UserSerializer
+#     permission_classes = [permissions.IsAdminUser]
